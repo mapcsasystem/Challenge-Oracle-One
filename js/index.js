9cc7285f8eb7ef59960eccf9571331bd9fda3ddd
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2022-08-29 12:26:32 -0500

    finish

--- js/index.js
+++ js/index.js
@@ -1,27 +1,96 @@
 
-
-// const textArea = document.getElementById("textArea");
-// // textArea.classList.add('textarea-transform');
-// textArea.addEventListener('keyup', valueInput);
-
-// function valueInput(e) {
-//     e.preventDefault();
-//     // const regExt = /([A-Z])\s+/ // Letras, numeros, guion y guion_bajo
-//     let value = e.target.value;
-//     for (let i = 0; i < value.length; i++) {
-//        console.log(`${i}: ${value[i]}`);
-//        value=value.toLowerCase();
-//     }
-//     e.target.value=value;
-// }
+let copiaTextArea = document.getElementById('copia-text-area')
+let copiaTextAreaValue = document.getElementById('copia-textarea')
+let copiarParrafo1 = document.getElementById('copiar-parrafo1');
+let copiarParrafo2 = document.getElementById('copiar-parrafo2');
+let buttonCopiar = document.getElementById('button-copiar');
+let muneco = document.getElementById('muneco');
 
 
-// const textarea1 = document.getElementById('encriptar');
+let buttonEncriptar = document.getElementById('button-encriptar');
+let buttonDesencriptar = document.getElementById('button-desencriptar');
+let encriptarTextarea = document.getElementById('encriptar-textarea');
 
-// textarea1.addEventListener('onkeypress', updateValue);
+function encriptar() {
+    if (encriptarTextarea.value.length === 0) {
+        alert("Inserta un valor.");
+        return null;
+    }
+    let text = encriptarTextarea.value;
+    if (text.includes('a') || text.includes('e') || text.includes('i') || text.includes('o') || text.includes('u')) {
+        copiaTextArea.style.display = "block"
+        copiaTextAreaValue.style.display = "block"
+        buttonCopiar.style.display = "block";
+        copiarParrafo1.style.display = "none";
+        copiarParrafo2.style.display = "none";
+        muneco.style.display = "none";
 
-// function updateValue(event) {
+        let textoConvertido = "";
+        for (let index = 0; index < text.length; index++) {
+            switch (text[index]) {
+                case 'a':
+                    textoConvertido = textoConvertido + "ai"
+                    break;
+                case 'e':
+                    textoConvertido = textoConvertido + "enter"
+                    break;
+                case 'i':
+                    textoConvertido = textoConvertido + "imes"
+                    break;
+                case 'o':
+                    textoConvertido = textoConvertido + "ober"
+                    break;
+                case 'u':
+                    textoConvertido = textoConvertido + "ufat"
+                    break;
+                default:
+                    textoConvertido = textoConvertido + text[index]
+                    break;
+            }
+        }
+        copiaTextAreaValue.value = textoConvertido;
+    } else {
+        alert("No existe ninguna vocal.");
+        copiaTextAreaValue.value = "";
+        copiaTextArea.style.display = "none"
+        copiaTextAreaValue.style.display = "none"
+        buttonCopiar.style.display = "none";
+        copiarParrafo1.style.display = "block";
+        copiarParrafo2.style.display = "block";
+        muneco.style.display = "block";
+        return null;
+    }
+}
 
-//     return (event.charCode >= 97 && event.charCode <= 122) ||  event.charCode===32 || event.charCode == 8 || event.charCode == 46 || event.charCode == 32
-//     // if(e.)
-// }
+function desencriptar() {
+    let text = copiaTextAreaValue.value;
+    if (text.includes('ai')) {
+        text = text.replaceAll('ai', 'a');
+    }
+    if (text.includes('enter')) {
+        text = text.replaceAll('enter', 'e');
+    }
+    if (text.includes('imes')) {
+        text = text.replaceAll('imes', 'i');
+    }
+
+    if (text.includes('ober')) {
+        text = text.replaceAll('ober', 'o');
+    }
+
+    if (text.includes('ufat')) {
+        text = text.replaceAll('ufat', 'u');
+    }
+
+    encriptarTextarea.value = text;
+    copiaTextAreaValue.value = text;
+}
+
+
+function copyEncriptar() {
+    copiaTextAreaValue.select();
+    copiaTextAreaValue.setSelectionRange(0, 99999); /* For mobile devices */
+    navigator.clipboard.writeText(copiaTextAreaValue.value);
+    encriptarTextarea.value = copiaTextAreaValue.value;
+    alert("Texto copiado: " + copiaTextAreaValue.value);
+}
